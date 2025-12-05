82e1ba5ba62af8d96d61c89161b2e4fe1c895722
anallaxz <lunna.devv@gmail.com>
2025-12-05 12:29:03 -0300

    first commit

---
+++ hipotenusa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    float cateto1, cateto2, hipotenusa;
+    printf ("Digite o primeiro valor:");
+    scanf ("%f", &cateto1);
+    printf ("Digite o segundo valor:");
+    scanf ("%f", &cateto2);
+    hipotenusa = sqrt ((cateto1 * cateto1) + (cateto2 * cateto2));
+    printf ("A hipotenusa é de: %.2f \n", hipotenusa);
+    return 0;
+}
